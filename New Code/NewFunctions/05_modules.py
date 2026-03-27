b5efb49fd8ee044af34fe37ac3cdbc2547714ed1
syamIT <syamdonkada255@gmail.com>
2026-03-27 17:22:50 +0530

    isadult in mymodule.py is created

--- New Code/NewFunctions/05_modules.py
+++ New Code/NewFunctions/05_modules.py
@@ -1,5 +1,5 @@
 import math as mh;
-print(mh.sqrt(25))
+print(mh.sqrt(26))
 print(mh.tan(45))
 print(mh.lcm(2,4,8))
 
@@ -11,3 +11,6 @@ print(mymodule.hello())
 # r = requests.get('https://w3schools.com/python/demopage.htm')
 # print(r.text)
 print(mymodule.hello())
+
+import mymodule2 as mm2;
+print(mm2.isadult(18))
---
+++ New Code/NewFunctions/mymodule2.py
@@ -0,0 +1,6 @@
+def isadult(age):
+    if(age > 18):
+        print("Adult")
+    else:
+        print("Not a Adult");
+    return age;
